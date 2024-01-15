ae524798d5bee260b9c46d01ab237f8131d0b5d3
Alphonse Datinnon <63641173+alphonse-datinnon@users.noreply.github.com>
2024-01-15 11:19:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-console.log("Hello ATCS BENIN, je suis dans console de chrome");
+document.addEventListener("DOMContentLoaded", function () {
+    // Sélectionnez les éléments nécessaires du DOM
+    var menuToggle = document.querySelector('.menu-toggle');
+    var nav = document.querySelector('header nav');
 
-var one 
+    // Ajoutez un écouteur d'événements au bouton de bascule du menu
+    menuToggle.addEventListener('click', function () {
+        // Basculez la classe 'active' pour afficher ou masquer le menu
+        nav.classList.toggle('active');
+    });
+
+    // Ajoutez un écouteur d'événements pour masquer le menu lors du clic sur un lien du menu
+    nav.addEventListener('click', function (event) {
+        // Si le menu est ouvert et un lien est cliqué, fermez le menu
+        if (nav.classList.contains('active') && event.target.tagName === 'A') {
+            nav.classList.remove('active');
+        }
+    });
+});
